b03f7b2857a1374ae997da8b50ad803900806b6c
Emilio Suri Lopez <surilopez@gmail.com>
2020-12-02 11:05:11 -0500

    Components for Theaters and movies was added

---
+++ src/app/Theaters/index-theater/index-theater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-theater',
+  templateUrl: './index-theater.component.html',
+  styleUrls: ['./index-theater.component.css']
+})
+export class IndexTheaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
