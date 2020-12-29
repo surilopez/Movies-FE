eb5f6d08310c78c729093bb4b59812402234b591
Emilio Suri Lopez <surilopez@gmail.com>
2020-12-29 12:00:54 -0500

    Funtion and views to list actors was added

--- src/app/Actors/index-actors/index-actors.component.ts
+++ src/app/Actors/index-actors/index-actors.component.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { ActorsService } from 'src/app/Services/actors.service';
+import { ActorDTO } from '../actor';
 
 @Component({
   selector: 'app-index-actors',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexActorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private actorsService : ActorsService
+  ) { }
+
+  actorsDTO: ActorDTO[] = []
+  displayedColumns: string[] = ['id', 'Name', 'Actions']
+  TotalRecords: any
+  ActualPage = 1;
+  recordsToShow = 10;
+
 
   ngOnInit(): void {
+    this.LoadRecord(this.ActualPage, this.recordsToShow)
+
+  }
+
+  LoadRecord(page: number, recordsToShow: number) {
+    this.actorsService.GetAllActors(page, recordsToShow).subscribe((response: HttpResponse<ActorDTO[]>) => {
+
+      this.actorsDTO = response.body!;
+      console.log(response.headers.get("TotalRecords"))
+      this.TotalRecords = response.headers.get("TotalRecords")!
+    }, error => {
+      console.error(error)
+    });
+  }
+
+  UpdatePagination(data: PageEvent) {
+    this.ActualPage = data.pageIndex + 1;
+    this.recordsToShow = data.pageSize;
+    this.LoadRecord(this.ActualPage, this.recordsToShow);
+  }
+
+  deleteGenre(id: number) {
+    this.actorsService.deleteActorByID(id)
+      .subscribe(() => {
+        this.LoadRecord(this.ActualPage, this.recordsToShow)
+      },error =>{
+        console.error(error)
+      })
   }
 
 }
--- src/app/Services/actors.service.ts
+++ src/app/Services/actors.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { ActorCreationDTO } from '../Actors/actor';
+import { ActorCreationDTO, ActorDTO } from '../Actors/actor';
 import { formatDate } from '../Utils/helpers';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,20 @@ export class ActorsService {
     private httpClient: HttpClient
   ) { }
 
+  public GetAllActors(page: number, recordsToShow: number): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('Page', page.toString())
+    params = params.append('RecordsByPage', recordsToShow.toString())
+    return this.httpClient.get<ActorDTO[] | null>(this.apiUrl, { observe: 'response', params })
+  }
+
   public AddNewActor(actor: ActorCreationDTO) {
     const formData = this.BuildFormData(actor);
     return this.httpClient.post(this.apiUrl, formData)
+  }
+
+  public deleteActorByID(id:number){
+    return this.httpClient.delete(`${this.apiUrl}/${id}`)
   }
 
   private BuildFormData(actor: ActorCreationDTO): FormData {
