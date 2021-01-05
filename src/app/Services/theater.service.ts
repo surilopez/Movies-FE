a2ad2cb663b9d06df7829d95b5eb5b429aef60e6
Emilio Suri Lopez <surilopez@gmail.com>
2021-01-05 11:35:57 -0500

    funtion for List and delete Theater was added

--- src/app/Services/theater.service.ts
+++ src/app/Services/theater.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { TheaterCreationDTO } from '../Theaters/theater';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';;
+import { TheaterCreationDTO, TheaterDTO } from '../Theaters/theater';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,16 @@ export class TheaterService {
   public AddNewTheater(theater: TheaterCreationDTO) {
     return this.httpClient.post(this.apiUrl, theater)
   }
+
+  public GetAllTheaters(page: number, recordsToShow: number): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('Page', page.toString())
+    params = params.append('RecordsByPage', recordsToShow.toString())
+    return this.httpClient.get<TheaterDTO[] | null>(this.apiUrl, { observe: 'response', params })
+  }
+
+  public deleteTheaterByID(id:number){
+    return this.httpClient.delete(`${this.apiUrl}/${id}`)
+  }
+
 }
--- src/app/Theaters/index-theater/index-theater.component.ts
+++ src/app/Theaters/index-theater/index-theater.component.ts
@@ -1,4 +1,9 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { GenreDTO } from 'src/app/Models/genre';
+import { TheaterService } from 'src/app/Services/theater.service';
+import { TheaterDTO } from '../theater';
 
 @Component({
   selector: 'app-index-theater',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexTheaterComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private theaterService: TheaterService) { }
+
+  theaterDTO: TheaterDTO[] = []
+  displayedColumns: string[] = ['id', 'Name', 'Actions']
+  TotalRecords: any
+  ActualPage = 1;
+  recordsToShow = 10;
+
 
   ngOnInit(): void {
+    this.LoadRecord(this.ActualPage, this.recordsToShow)
+
+  }
+
+  LoadRecord(page: number, recordsToShow: number) {
+    this.theaterService.GetAllTheaters(page, recordsToShow).subscribe((response: HttpResponse<TheaterDTO[]>) => {
+
+      this.theaterDTO = response.body!;
+
+      this.TotalRecords = response.headers.get("TotalRecords")!
+    }, error => {
+      console.error(error)
+    });
+  }
+
+  UpdatePagination(data: PageEvent) {
+    this.ActualPage = data.pageIndex + 1;
+    this.recordsToShow = data.pageSize;
+    this.LoadRecord(this.ActualPage, this.recordsToShow);
+  }
+
+  deleteGenre(id: number) {
+    this.theaterService.deleteTheaterByID(id)
+      .subscribe(() => {
+        this.LoadRecord(this.ActualPage, this.recordsToShow)
+      },error =>{
+        console.error(error)
+      })
   }
 
 }
